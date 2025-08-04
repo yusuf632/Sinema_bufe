51544c161185f1af6bf949e493563016144f6843
yusuf632 <yusufislamdeligoz333@gmail.com>
2025-08-04 11:02:10 +0300

    Güncellendi.

--- Sinema_bufe/Form1.Designer.cs
+++ Sinema_bufe/Form1.Designer.cs
@@ -30,27 +30,27 @@ namespace Sinema_bufe
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -77,62 +77,11 @@ namespace Sinema_bufe
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ürün Miktar";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(15, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mısır:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(77, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 29);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(77, 82);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 29);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(36, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Su:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(77, 123);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 29);
-            this.textBox3.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(27, 126);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 22);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Çay:";
-            // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(77, 163);
+            this.textBox4.Location = new System.Drawing.Point(89, 163);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 29);
+            this.textBox4.Size = new System.Drawing.Size(100, 34);
             this.textBox4.TabIndex = 7;
             // 
             // label4
@@ -141,9 +90,60 @@ namespace Sinema_bufe
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(19, 166);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 22);
+            this.label4.Size = new System.Drawing.Size(64, 27);
             this.label4.TabIndex = 6;
             this.label4.Text = "Bilet:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 34);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(27, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Çay:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 34);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(36, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Su:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mısır:";
             // 
             // groupBox2
             // 
@@ -159,45 +159,45 @@ namespace Sinema_bufe
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Fiyat Tablosu";
             // 
-            // label5
+            // label8
             // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(16, 40);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 22);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Mısır: 10TL";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(47, 85);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 22);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Su: 1TL";
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(9, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 27);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Bilet: 8TL";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(38, 126);
+            this.label7.Location = new System.Drawing.Point(9, 123);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 22);
+            this.label7.Size = new System.Drawing.Size(96, 27);
             this.label7.TabIndex = 2;
             this.label7.Text = "Çay: 2TL";
             // 
-            // label8
+            // label6
             // 
-            this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(30, 166);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(86, 22);
-            this.label8.TabIndex = 3;
-            this.label8.Text = "Bilet: 8TL";
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(9, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 27);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Su: 1TL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(9, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 27);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Mısır: 10TL";
             // 
             // groupBox3
             // 
@@ -215,47 +215,16 @@ namespace Sinema_bufe
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Kasa";
             // 
-            // label9
+            // button2
             // 
-            this.label9.AutoSize = true;
-            this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(17, 40);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(77, 22);
-            this.label9.TabIndex = 0;
-            this.label9.Text = "Toplam:";
-            // 
-            // button1
-            // 
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(21, 103);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Hesapla";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(177, 9);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(370, 30);
-            this.label10.TabIndex = 3;
-            this.label10.Text = "Sinema & Büfe Satış Yeri";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(100, 40);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(49, 22);
-            this.label11.TabIndex = 2;
-            this.label11.Text = "00TL";
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(141, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 47);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label12
             // 
@@ -263,7 +232,7 @@ namespace Sinema_bufe
             this.label12.ForeColor = System.Drawing.Color.White;
             this.label12.Location = new System.Drawing.Point(77, 166);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(49, 22);
+            this.label12.Size = new System.Drawing.Size(60, 27);
             this.label12.TabIndex = 4;
             this.label12.Text = "00TL";
             // 
@@ -273,20 +242,51 @@ namespace Sinema_bufe
             this.label13.ForeColor = System.Drawing.Color.White;
             this.label13.Location = new System.Drawing.Point(17, 166);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(52, 22);
+            this.label13.Size = new System.Drawing.Size(66, 27);
             this.label13.TabIndex = 3;
             this.label13.Text = "Kasa:";
             // 
-            // button2
+            // label11
             // 
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(141, 103);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 29);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Temizle";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(100, 40);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 27);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "00TL";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(21, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(17, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 27);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Toplam:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(177, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(469, 36);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Sinema & Büfe Satış Yeri";
             // 
             // panel1
             // 
@@ -299,16 +299,16 @@ namespace Sinema_bufe
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateBlue;
-            this.ClientSize = new System.Drawing.Size(715, 318);
+            this.ClientSize = new System.Drawing.Size(715, 323);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Sinema Büfe";
             this.groupBox1.ResumeLayout(false);
